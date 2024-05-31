e07ffc90904519cbc9dbcba6a5d0186922899d6f
Developer <mjat09@outlook.com>
2024-05-31 17:33:07 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,29 @@
+import time
+import random
+
+
+print("Game [Version: 0.01].")
+print("This game was made with python.")
+
+time.sleep(0.5)  # Add a 3-second delay
+print("game on.")
+time.sleep(0.7)  # Add a 3-second delay
+print('-'* 150)
+
+
+
+print("")
+time.sleep(0.7)  # Add a 3-second delay
+print(" ██████╗  █████╗ ███╗   ███╗███████╗")
+time.sleep(0.8)  # Add a 3-second delay
+print("██╔════╝ ██╔══██╗████╗ ████║██╔════╝")
+time.sleep(0.9)  # Add a 3-second delay
+print("██║  ███╗███████║██╔████╔██║█████╗  ")
+time.sleep(0.10)  # Add a 3-second delay
+print("██║   ██║██╔══██║██║╚██╔╝██║██╔══╝  ")
+time.sleep(0.11)  # Add a 3-second delay
+print("╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗")
+time.sleep(0.12)  # Add a 3-second delay
+print(" ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝")
+print("")
+    
